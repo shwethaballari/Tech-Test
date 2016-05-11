cd48985d7fd8ce4704456109c4b9faa682962e36
shwethaballari <shwetha.ballari@sainsburys.co.uk>
2016-05-11 11:06:23 +0100

    Add files via upload

---
+++ Tech-cookbooks/nginx/metadata.rb
@@ -0,0 +1,9 @@
+name              'nginx'
+maintainer        'Mike Fiedler'
+maintainer_email  'miketheman@gmail.com'
+license           'Apache 2.0'
+description       'Installs and configures nginx'
+version           '2.99.0'
+
+issues_url 'https://github.com/miketheman/nginx/issues'
+source_url 'https://github.com/miketheman/nginx'
